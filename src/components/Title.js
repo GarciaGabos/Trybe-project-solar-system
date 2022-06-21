4f38790fbe82bb5f002b649bc0eca6082c110be5
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-21 16:17:38 -0300

    Travado no 3 e 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.component {
   render() {
+    const { headline } = this.props;
     return (
-      <h2>{this.props.headline}</h2>
+      <h2>{ headline }</h2>
     );
   }
 }
 
 export default Title;
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
