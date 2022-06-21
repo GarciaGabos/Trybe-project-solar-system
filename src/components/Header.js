1f600dabac5bf5bc35761ac9b67324be847aed8d
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-21 14:52:25 -0300

    Instalado node correto e requisito 1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Header extends React.component {
+    render() {
+        return (
+            <header> 
+                <h1>Sitema Solar</h1>
+            </header>
+        );
+    }
+
+}
+
+export default Header
