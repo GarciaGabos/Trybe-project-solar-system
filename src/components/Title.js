161f892c324d15d631c14b699c7fa78a40cda2e5
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-21 17:31:07 -0300

    Ate requisito 8 feito

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Title extends React.component {
+class Title extends React.Component {
   render() {
     const { headline } = this.props;
     return (
@@ -10,8 +10,8 @@ class Title extends React.component {
   }
 }
 
-export default Title;
-
 Title.propTypes = {
   headline: PropTypes.string.isRequired,
 };
+
+export default Title;
